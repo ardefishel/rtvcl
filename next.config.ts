2261dab1387a5ab3753390bb9d952c2bcfd6c663
rtvcl // wira <ardefishel@RTVCL---Macbook-Pro-M4.local>
2026-03-05 11:50:31 +0700

    deployment: add rewrite to laughtrack landing page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: '/laughtrack',
+        destination: 'https://laughtrack-web.rtvcl.com',
+      },
+      {
+        source: '/laughtrack/:path*',
+        destination: 'https://laughtrack-web.rtvcl.com/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
